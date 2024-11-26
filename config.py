98c1bba8f92befa690f175f957e33b629d9fd13c
Gabriel Oget <oget.gabriel@gmail.com>
2024-11-26 08:13:20 +0100

    Cleaning Traffic Data

--- config.py
+++ config.py
@@ -5,3 +5,4 @@ PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 
 # Define common paths relative to the project root
 TRACKER_PATH = os.path.join(PROJECT_ROOT, "data", "api", "request_tracker.json")
+DATA_PATH = os.path.join(PROJECT_ROOT, "data")
