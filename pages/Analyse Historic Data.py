e78bbf06a26ec777e0fd66be5c9ae8bdd5d2651f
Gabriel Oget <oget.gabriel@gmail.com>
2024-12-08 23:29:11 +0100

    changed name

--- pages/Analyse Historic Data.py
+++ pages/Analyse Historic Data.py
@@ -31,7 +31,7 @@ MAP_BOUNDS = {
 # --- Load Data ---
 @st.cache_data
 def load_data():
-    df = pd.read_csv("data/clean/merged.csv")
+    df = pd.read_csv("data/inference/historic_data.csv")
     return df
 
 @st.cache_data
